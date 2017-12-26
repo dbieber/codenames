1efd07e115fce01cb782da155314588b3063c25e
Adriana Cherskov <adriana.cherskov@gmail.com>
2017-12-26 00:03:21 -0800

    randomize first player

--- codenames.py
+++ codenames.py
@@ -18,11 +18,15 @@ DARK = (30, 30, 30)
 
 x = range(25)
 random.shuffle(x)
+first_player = random.randint(0, 1)
 
-blue = x[:8]
-red = x[8:8+9]
-assassin = x[8+9]
-neutral = x[8+9+1:]
+num_blue = 8 + first_player
+num_red = 9 - first_player
+blue = x[:num_blue]
+red = x[num_blue:num_blue + num_red]
+assassin = x[num_blue+num_red]
+neutral = x[num_blue+num_red+1:]
+
 
 def new_image_draw(size):
   image = Image.new(
@@ -39,20 +43,20 @@ def square(draw, r, c, color):
   draw.rectangle((r * w, c * w, (r + 1) * w, (c + 1) * w), fill=color, outline=DARK)
 
 def generate():
-	image, draw = new_image_draw((5*w, 5*w))
-	for row in range(5):
-	  for col in range(5):
-	    index = 5 * row + col
-	    if index == assassin:
-	      square(draw, row, col, BLACK)
-	    elif index in red:
-	      square(draw, row, col, RED)
-	    elif index in blue:
-	      square(draw, row, col, BLUE)
-	    elif index in neutral:
-	      square(draw, row, col, SNOW)
+  image, draw = new_image_draw((5*w, 5*w))
+  for row in range(5):
+    for col in range(5):
+      index = 5 * row + col
+      if index == assassin:
+        square(draw, row, col, BLACK)
+      elif index in red:
+        square(draw, row, col, RED)
+      elif index in blue:
+        square(draw, row, col, BLUE)
+      elif index in neutral:
+        square(draw, row, col, SNOW)
 
-	image.save('codenames.png')
+  image.save('codenames.png')
 
 if __name__ == '__main__':
-	generate()
+  generate()
